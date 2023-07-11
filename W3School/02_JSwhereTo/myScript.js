9131dce3958f349f52e0d6545824cbfa72d0ff55
Andrea Barraco <andreabarraco1695@gmail.com>
2023-07-11 15:50:07 +0200

    Commit

---
+++ W3School/02_JSwhereTo/myScript.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    //all'elemento con id "demo" andrò ad applicare tale modifica
+    document.getElementById("demo").innerHTML = "Paragraph changed";
+}
+
+function myFunction2(){
+    //all'elemento con id "demo" andrò ad applicare tale modifica
+    document.getElementById("demo").innerHTML = "A paragraph";
+}
